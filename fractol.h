0d30ffbf2635cb34cfeee2bf5180b1ff0c18cbc3
mollymoon2b <mollymoon2b@gmail.com>
2015-01-12 00:19:26 +0100

    alice

--- fractol.h
+++ fractol.h
@@ -72,9 +72,6 @@ void			ft_error(char *s);
 int				ft_exposehook(t_env *e);
 int				keyboard_event(int keycode, t_env *e);
 int				mouse_event(int button, int x, int y, t_env *e);
-// int				draw_mandelbrot(t_env *e);
-// int				draw_julia(t_env *e);
-
 void			ft_put_pixel_to_img(t_buffer *buffer, int x, int y, t_color color);
 t_color			ft_tohex(int h, float s, float l);
 t_color			ft_get_color(char r, char g, char b);
