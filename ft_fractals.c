f9fc8510ca9df6fb9f2f45008c871f209ca6c1c8
achazal <arthur.chazal@gmail.com>
2015-01-12 03:59:26 +0100

    Wheeeee

--- ft_fractals.c
+++ ft_fractals.c
@@ -28,6 +28,8 @@ int		ft_julia(int max_it, t_complex z, t_complex c)
 		tmp.real = tmp.real * tmp.real - tmp.imag * tmp.imag + c.real;
 		tmp.imag = (2 * x * tmp.imag) + c.imag;
 	}
+	// if (i)
+		// printf("Color = %i\n", i);
 	return (i);
 }
 
@@ -58,6 +60,20 @@ void		ft_screenloop(t_env *e, t_complex z)
 	int			y;
 	t_complex	c;
 
+	// e->init.real = 0;
+	// e->init.imag = 0;
+	// e->steps.real = 0.002497;
+	// e->steps.imag = 0.002497;
+
+	c.real = e->init.real;
+	c.imag = e->init.imag;
+
+	// x = 0;
+	// y = 0;
+	// c.imag = (x - e->offset.x) * (1 / (e->zoom * 5));
+	// c.real = (y - e->offset.y) * (1 / (e->zoom * 5));
+	printf("Init x = %f, init y = %f\n", c.imag, c.real);
+	// printf("Steps x = %f, steps y = %f\n", e->steps.imag, e->steps.real);
 	y = 0;
 	c.real = e->init.real;
 	while (y < WIN_HEIGHT)
@@ -66,17 +82,19 @@ void		ft_screenloop(t_env *e, t_complex z)
 		c.imag = e->init.imag;
 		while (x < WIN_WIDTH)
 		{
-			// c.real = (x - e->offset.x) * (1 / (e->zoom * 5));
-			// c.imag = (y - e->offset.y) * (1 / (e->zoom * 5));
-			c.imag += e->steps.imag;
+			// c.imag = (x - e->offset.x) * (1 / (e->zoom * 5));
+			// c.real = (y - e->offset.y) * (1 / (e->zoom * 5));
 			i = e->fractal(e->max_it, c, z);
 			ft_put_pixel_to_img(&e->buffer, x, y,
 				ft_tohex(i % 360, 1, 0.5 * (i < e->max_it)));
 			x++;
+			c.imag += e->steps.imag;
 		}
 		y++;
 		c.real += e->steps.real;
 	}
+	printf("Max x = %f, max y = %f\n", c.imag / 2, c.real / 2);
+	printf("Steps x = %f, steps y = %f\n", c.imag / WIN_HEIGHT, c.real / WIN_HEIGHT);
 }
 
 int				ft_init_mandelbrot(t_env *e)
@@ -92,13 +110,21 @@ int				ft_init_mandelbrot(t_env *e)
 
 int				ft_init_julia(t_env *e)
 {
-	e->z.real = -0.8;
-	e->z.imag = 0.156;
-	e->steps.real = 0.01;
-	e->steps.imag = 0.01;
-	e->center.real = 0;
-	e->center.imag = 0;
+	e->max_it = 10000;
+	e->z.imag = -0.70176;
+	e->z.real = -0.3842;
+	e->steps.real = 0.002497;
+	e->steps.imag = 0.002497;
+	e->center.real = 1.872750;
+	e->center.imag = 1.248500;
 	ft_setinit(e);
+	e->fractal = ft_julia;
+	return (0);
+
+	e->z.imag = -0.70176;
+	e->z.real = -0.3842;
+	e->max_it = 10000;
+	e->zoom = 80;
 	e->fractal = ft_julia;
 	return (0);
 }
--- ft_keypress.c
+++ ft_keypress.c
@@ -27,8 +27,8 @@ int		ft_exposehook(t_env *e)
 
 void	ft_setinit(t_env *e)
 {
-	e->init.real = e->center.real - WIN_WIDTH * e->steps.real;
-	e->init.imag = e->center.imag - WIN_HEIGHT * e->steps.imag;
+	e->init.real = e->center.real - WIN_HEIGHT / 2 * e->steps.real;
+	e->init.imag = e->center.imag - WIN_WIDTH / 2 * e->steps.imag;
 }
 
 int		keyboard_event(int keycode, t_env *e)
@@ -49,13 +49,15 @@ int		keyboard_event(int keycode, t_env *e)
 		e->center.imag -= e->steps.imag * (WIN_HEIGHT / 10);
 	if (keycode == RIGHT)
 		e->center.imag += e->steps.imag * (WIN_HEIGHT / 10);
-	if (keycode == LESS)
+	if (keycode == MORE)
 	{
+		printf("More\n");
 		e->steps.real *= 9 / 10.0;
 		e->steps.imag *= 9 / 10.0;
 	}
-	if (keycode == MORE)
+	if (keycode == LESS)
 	{
+		printf("Less\n");
 		e->steps.real *= 10 / 9.0;
 		e->steps.imag *= 10 / 9.0;
 	}
@@ -63,11 +65,55 @@ int		keyboard_event(int keycode, t_env *e)
 	{
 		ft_setinit(e);
 		ft_screenloop(e, e->z);
-		printf("Center x = %f, center y = %f\n", e->init.imag, e->init.real);
-		printf("Steps x = %f, steps y = %f\n", e->steps.imag, e->steps.real);
+		// printf("Center x = %f, center y = %f\n", e->init.imag, e->init.real);
+		// printf("Steps x = %f, steps y = %f\n", e->steps.imag, e->steps.real);
 		mlx_put_image_to_window(e->mlx, e->win, e->buffer.img, 0, 0);
 	}
 	return (0);
+
+	// int update;
+
+	// update = 0;
+	// (void)e;
+	// if (keycode == 65307)
+	// 	exit(0);
+	// if (keycode == 65362)
+	// {
+	// 	e->offset.y += e->zoom / 8;
+	// 	update = 1;
+	// }
+	// if (keycode == 65364)
+	// {
+	// 	e->offset.y -= e->zoom / 8;
+	// 	update = 1;
+	// }
+	// if (keycode == 65361)
+	// {
+	// 	e->offset.x += e->zoom / 8;
+	// 	update = 1;
+	// }
+	// if (keycode == 65363)
+	// {
+	// 	e->offset.x -= e->zoom / 8;
+	// 	update = 1;
+	// }
+	// if (keycode == 45)
+	// {
+	// 	e->zoom *= 9 / 10.0;
+	// 	update = 1;
+	// }
+	// if (keycode == 61)
+	// {
+	// 	e->zoom *= 10 / 9.0;
+	// 	update = 1;
+	// }
+	// if (update)
+	// {
+	// 	printf("Updating\n");
+	// 	ft_screenloop(e, e->z);
+	// 	mlx_put_image_to_window(e->mlx, e->win, e->buffer.img, 0, 0);
+	// }
+	// return (0);
 }
 
 int		mouse_event(int button, int x, int y, t_env *e)
