919d38f6c4f8b82c7727014380831305a7b83bfb
mollymoon2b <mollymoon2b@gmail.com>
2015-01-17 14:46:55 +0100

    fini

--- ft_init_fract.c
+++ ft_init_fract.c
@@ -6,7 +6,7 @@
 /*   By: ade-bonn <ade-bonn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/15 12:22:36 by ade-bonn          #+#    #+#             */
-/*   Updated: 2015/01/15 12:22:36 by ade-bonn         ###   ########.fr       */
+/*   Updated: 2015/01/17 14:41:33 by ade-bonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,8 +43,8 @@ int		ft_init_fractoil(t_env *e)
 	e->max_it = 100;
 	e->z.imag = -0.75;
 	e->z.real = -0.00;
-	e->steps.real = 0.001;
-	e->steps.imag = 0.001;
+	e->steps.real = 0.01;
+	e->steps.imag = 0.01;
 	e->center.real = 0;
 	e->center.imag = 0;
 	ft_setinit(e);
@@ -57,8 +57,8 @@ int		ft_init_dragon(t_env *e)
 	e->max_it = 100;
 	e->z.imag = -0.45186;
 	e->z.real = -0.0984651;
-	e->steps.real = 0.001;
-	e->steps.imag = 0.001;
+	e->steps.real = 0.01;
+	e->steps.imag = 0.01;
 	e->center.real = 0;
 	e->center.imag = 0;
 	ft_setinit(e);
